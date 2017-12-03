87f5adf487e2ae67a09112805b914cd47fa6023e
Jan Vomlel <jan.vomlel@aipsafe.cz>
2017-12-03 15:21:51 +0100

    day3

---
+++ src/Day3.kt
@@ -0,0 +1,35 @@
+package day3
+import java.lang.Math.abs
+
+fun main(args: Array<String>) {
+
+    do {
+        //Read value
+        val line = readLine()
+
+        if (line != null) {
+
+            val value = line.toInt()
+
+            val steps = if (value == 1) 0 else {
+                //Find the square size
+                var n = 1;
+                while (n*n < value) n += 2
+
+                //find order in the largest square
+                val m = value - (n-2)*(n-2)
+
+                //position on the side
+                val l = m % (n-1)
+
+                //Steps on the side+steps to the center
+                abs(l - (n-1)/2)+(n-1)/2
+            }
+            println(steps)
+        }
+
+
+    } while (line != null)
+
+
+}
---
+++ src/Day3Part2.kt
@@ -0,0 +1,56 @@
+package day3part2
+import java.lang.Math.abs
+
+data class Indices(val i1: Int, val i2: Int) {
+
+    fun next(): Indices {
+        if (abs(i1) > abs(i2)) {
+            if (i1 > 0) return Indices(i1, i2+1)
+            else return Indices(i1, i2-1)
+        } else if (abs(i1) < abs(i2)) {
+            if (i2 > 0) return Indices(i1-1, i2)
+            else return Indices(i1+1, i2)
+        } else {
+            if (i1 > 0 && i2 > 0) return Indices(i1-1, i2)
+            else if (i1 > 0) return Indices(i1+1, i2)
+            else if (i2 > 0) return Indices(i1, i2-1)
+            else return Indices(i1+1, i2)
+        }
+    }
+
+    fun neghbours() = listOf(
+            Indices(i1-1, i2),
+            Indices(i1-1, i2-1),
+            Indices(i1-1, i2+1),
+            Indices(i1+1, i2),
+            Indices(i1+1, i2-1),
+            Indices(i1+1, i2+1),
+            Indices(i1, i2-1),
+            Indices(i1, i2+1))
+}
+
+fun sumTo(stopSum: Int): Int {
+    val sumMap = mutableMapOf<Indices, Int>()
+    var currentIndices = Indices(0,0)
+    var currentValue = 1
+    sumMap[currentIndices] = currentValue
+    while (currentValue <= stopSum) {
+        currentIndices = currentIndices.next()
+        currentValue = currentIndices.neghbours().map { sumMap[it] ?: 0 }.sumBy { it }
+        sumMap[currentIndices] = currentValue
+    }
+    return currentValue;
+}
+
+fun main(args: Array<String>) {
+
+    do {
+        val line = readLine()
+        if (line != null) {
+
+            val value = line.toInt()
+            val sum = sumTo(value)
+            println(sum)
+        }
+    } while (line != null)
+}
