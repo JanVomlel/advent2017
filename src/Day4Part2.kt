cd9191d5a3e2970c287316c38f0e42c4bc08c0f7
Jan Vomlel <jan.vomlel@aipsafe.cz>
2017-12-04 23:10:21 +0100

    day4

--- src/Day4Part2.kt
+++ src/Day4Part2.kt
@@ -1,11 +1,11 @@
-package day4
+package day4part2
 
 fun main(args: Array<String>) {
     var count = 0
     do {
         val line = readLine()
-        if (line != null) {
-            val words = line.split(' ').filter{!it.isEmpty()}
+        if (line != null && !line.isEmpty()) {
+            val words = line.split(' ').filter{!it.isEmpty()}.map {it.toList().sorted()}
             val wordsSet = words.toSet()
             if (words.size == wordsSet.size) count++
         }
